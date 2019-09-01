e0a03cb3f296354af4a83e8b120bd2f3980dd13a
Devin-Bielejec <devin.bielejec@gmail.com>
2019-09-01 11:10:51 -0600

    Dark Mode works, mistake was forgetting to pass a cb into useEffect

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,11 @@
+import { useLocalStorage } from "./useLocalStorage";
+import { useEffect } from "react";
+
+export const useDarkMode = () => {
+    const [darkMode, setDarkMode] = useLocalStorage("dark");
+    useEffect( () => {
+        darkMode === true ? document.querySelector("body").classList.add("dark-mode") : document.querySelector("body").classList.remove("dark-mode")
+    }, [darkMode])
+    return [darkMode, setDarkMode];
+}
+
